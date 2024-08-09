b09b89e07f74a212a96f1e1bfff7b9efdd5f16e8
ojm-derivatives <ojm.derivatives@gmail.com>
2024-08-09 21:14:16 +0530

    Working Stable.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-import os
+from dotenv import load_dotenv
+load_dotenv()
 
-from com.prod.util import *
+import rms
+from com.prod.util import wait_until_entry_time
 from rms import RMS_Position_Order_Matching
 from rms import RMS_GenerateExpiryCodes
-import rms
 import concurrent.futures
+from com.decorators import check_holiday
 
 
 
@@ -12,12 +14,13 @@ import concurrent.futures
 def run_main_method_of_sub_module(obj):
     obj.main()
 
+check_holiday("RMS_Main")
 def run_main():
     entry_datetime = rms.rms_main_obj.time_details_obj.algo_run_start_time_datetime
     wait_until_entry_time(entry_datetime=entry_datetime, interval=1)
 
-    sub_modules = {'rms_pos_match': RMS_Position_Order_Matching('Endurance', 'RMS'),
-                   'rms_gen_exp_codes': RMS_GenerateExpiryCodes('Endurance', 'RMS')
+    sub_modules = {'rms_pos_match': RMS_Position_Order_Matching(module_name='RMS_Pos_Match'),
+                   'rms_gen_exp_codes': RMS_GenerateExpiryCodes(module_name='RMS_Generate_Expiry_Codes')
                    }
     rms.rms_main_obj.main()
     rms.rms_m2m_obj.main()
@@ -33,20 +36,7 @@ def run_main():
             except Exception as exc:
                 print(f'Generated an exception: {exc}')
 
-    # with concurrent.futures.ProcessPoolExecutor() as executor:
-    #
-    #     # Submit all main method calls to the executor
-    #     futures = [executor.submit(run_main_method_of_sub_module, obj) for obj in sub_modules.values()]
-    #
-    #     # Optionally wait for all futures to complete
-    #     for future in concurrent.futures.as_completed(futures):
-    #         try:
-    #             future.result()  # If you need to handle exceptions, access the result here
-    #         except Exception as exc:
-    #             print(f'Generated an exception: {exc}')
-
     print("All main methods have been called.")
-
 
 if __name__ == '__main__':
     run_main()
